a07810a4e1e1245ec0cecb870c1bfdd314fdcc92
niftymaestro <n109m89@outlook.com>
2021-06-11 16:20:59 +0700

    FractalPiece2D initialisation from data

--- FractalPiece2D.py
+++ FractalPiece2D.py
@@ -1,12 +1,29 @@
 from Vector2D import Vector2D
 from Matrix2D import Matrix2D
 
+dummyVect = Vector2D()
+dummyMx = Matrix2D()
+
 
 class FractalPiece2D:
-    def __init__(self):
-        self.id = 0
-        self.vect = Vector2D()
-        self.mx = Matrix2D()
+    def __init__(self, id=0, vect=dummyVect, mx=dummyMx):
+        if isinstance(id, int):
+            if id >= 0:
+                self.id = id
+            else:
+                self.id = 0
+        else:
+            self.id = 0
+        
+        if isinstance(vect, Vector2D):
+            self.vect = vect
+        else:
+            self.vect = Vector2D() # need new object here, not the dummy object
+
+        if isinstance(mx, Matrix2D):
+            self.mx = mx
+        else:
+            self.mx = Matrix2D() # need new object
 
     def copy(self):
         result = FractalPiece2D()
