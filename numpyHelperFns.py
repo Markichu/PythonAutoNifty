c759675499d95edfd7c4ad5910c90fb9348a9740
niftymaestro <n109m89@outlook.com>
2021-07-14 12:36:30 +0700

    Add new metric

--- numpyHelperFns.py
+++ numpyHelperFns.py
@@ -19,6 +19,11 @@ def vect(x=0, y=0, z=None, scale=1):
 def vect_len(vect):
     return np.sum(vect * vect) ** 0.5
 
+# Find a metric for matrix using length of transformation of x-coord (1, 0)
+# Currently 2D only
+def metric_matrix_x_coord(mx):
+    return (mx[0][0]**2 + mx[1][0]**2) ** 0.5
+
 # Find a metric for matrix using square root of sum of squares of matrix entries
 # Identity matrix in 2D or 3D has metric 1
 def metric_matrix_rms(mx):
